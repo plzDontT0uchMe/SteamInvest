a167c3ae30e9c975834db4244b9bc1e8bdf33f04
plzDontT0uchMe <plzDontT0uchMe@proton.me>
2024-03-12 21:01:56 +0300

    Bag fixed

--- src/components/menuItems/MyAccount.vue
+++ src/components/menuItems/MyAccount.vue
@@ -4,12 +4,11 @@ import AvatarPreview from "@/components/menuPreviews/AvatarPreview.vue";
 import NewAvatarIcon from "@/components/menuIcons/NewAvatarIcon.vue";
 import axios from "@/axios";
 
-const newImageAvatar = ref();
-const imagesAvatar = ref([
+const images = ref([
     {
         id: 0,
         url: '/src/assets/images/1.jpg',
-        isActive: true
+        isActive: false
     },
     {
         id: 3,
@@ -38,51 +37,59 @@ const imagesAvatar = ref([
     }
 ]);
 
+const selectedImage = ref(0);
+
 onMounted(() => {
-    initImagesAvatar();
+    initImages();
 })
 
-const indexImagesAvatar = ref(0);
-const countDisplayImagesAvatar = 3;
-const initImagesAvatar = () => {
-    imagesAvatar.value.unshift({id:-1, isActive: false, createAvatar: true});
-    updateIndexImageAvatar();
-    imagesAvatar.value.push({id:-2, isActive: false, emptyAvatar: true});
+const indexDisplayImage = ref(0);
+const countDisplayImages = 3;
+const initImages = () => {
+    images.value.unshift({id:-1, isActive: false, create: true});
+    selectedImage.value = images.value.find((item) => item.isActive);
+    images.value.push({id:-2, isActive: false, empty: true});
+    updateIndexImage();
 }
-const updateIndexImageAvatar = () => {
-    indexImagesAvatar.value = imagesAvatar.value.indexOf(imagesAvatar.value.find((item) => item.isActive === true));
+const updateIndexImage = () => {
+    indexDisplayImage.value = images.value.indexOf(selectedImage.value);
+    if(indexDisplayImage.value === -1) {
+        if(images.value.length === 2)
+            indexDisplayImage.value = -1;
+        else
+            indexDisplayImage.value = 1;
+    }
 }
 const displayImagesAvatar = computed(() => {
-    if(indexImagesAvatar.value !== 0)
-        return imagesAvatar.value.slice(indexImagesAvatar.value - 1, indexImagesAvatar.value - 1 + countDisplayImagesAvatar);
+    if(indexDisplayImage.value !== 0)
+        return images.value.slice(indexDisplayImage.value - 1, indexDisplayImage.value - 1 + countDisplayImages);
     else
-        return imagesAvatar.value.slice(indexImagesAvatar.value, indexImagesAvatar.value + countDisplayImagesAvatar - 1);
+        return images.value.slice(indexDisplayImage.value, indexDisplayImage.value + countDisplayImages - 1);
 });
 
 const myFile = ref();
 const loadImage = (event) => {
     const file = event.target.files[0];
-    console.log(file)
-    imagesAvatar.value.splice(1,0,{
+    const img = {
         url: URL.createObjectURL(file),
         file: file,
         isLoading: true,
         progress: 0
-    });
+    }
+    images.value.splice(1,0, img);
+    setImage(img);
     myFile.value.value = ''
-    console.log(imagesAvatar.value)
 }
 const setImage = (img) => {
-    imagesAvatar.value.forEach((item) => {
-        item.isActive = item.id === img.id;
-    })
-    updateIndexImageAvatar();
+    selectedImage.value = img;
+    updateIndexImage();
 }
-const deleteImageAvatar = (img) => {
-    imagesAvatar.value = imagesAvatar.value.filter((item) => item.url !== img.url)
+const deleteImage = (img) => {
+    images.value = images.value.filter((item) => item.url !== img.url)
+    console.log(indexDisplayImage.value)
+    updateIndexImage();
 }
-const downloadImageAvatar = async (img) => {
-    console.log(img)
+const downloadImage = async (img) => {
     const data = new FormData();
     data.append('asset', img.file);
     data.append('type', 'avatar');
@@ -107,9 +114,9 @@ const downloadImageAvatar = async (img) => {
         <div class="flex flex-row justify-around items-center w-full select-none">
 
             <button class="text-3xl hover:scale-110 hover:opacity-50 transition-all delay-75"
-                    :disabled="indexImagesAvatar === 1 || Object.keys(imagesAvatar).length === 2"
-                    :class="{'opacity-0' : indexImagesAvatar === 1 || Object.keys(imagesAvatar).length === 2}"
-                    @click="indexImagesAvatar--"
+                    :disabled="indexDisplayImage === 1 || Object.keys(images).length === 2"
+                    :class="{'opacity-0' : indexDisplayImage === 1 || Object.keys(images).length === 2}"
+                    @click="indexDisplayImage--"
             >
                 <
             </button>
@@ -121,38 +128,32 @@ const downloadImageAvatar = async (img) => {
                     v-for="img in displayImagesAvatar"
                     :key="img.id"
                 >
-                    <div v-if="img?.createAvatar" @click="myFile.click" class="btn btn-square btn-outline btn-sm flex justify-center items-center w-32 h-32">
+                    <div v-if="img?.create" @click="myFile.click" class="btn btn-square btn-outline btn-sm flex justify-center items-center w-32 h-32">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
                     </div>
-                    <div v-else-if="img?.emptyAvatar" class="w-32 h-32"></div>
-                    <div v-else-if="img?.isLoading" class="w-32 h-32">
-                        <img class="w-32 h-32 cursor-pointer"
+                    <div v-else-if="img?.empty" class="w-32 h-32"></div>
+                    <div v-else class="relative">
+                        <img class="w-32 h-32 cursor-pointer hover:scale-110 hover:opacity-50 transition-all delay-75"
                              :src="img.url"
-                             :class="{'scale-110 outline': img.isActive}"
+                             :class="{'scale-110 outline': img === selectedImage}"
                              @click="setImage(img)"
                         >
-                        <div class="flex justify-between items-center w-full h-10 mt-3">
-                            <div class="relative w-full w-[70%] h-8">
-                                <button class="btn btn-outline btn-success btn-sm z-1" @click="downloadImageAvatar(img)">Загрузить</button>
-                                <div v-if="img?.isLoading && img?.progress !== 100" class="absolute left-0 top-0 h-full bg-[#00A872] rounded-lg z-[-999] transition-all duration-1000 pr-5" :style="{width: img?.progress + '%'}" ></div>
-                            </div>
-                            <button class="btn btn-square btn-outline btn-error btn-sm w-[25%]" @click="deleteImageAvatar(img)">
+                        <div v-if="img?.isLoading" class="absolute left-0 flex justify-between items-center w-full h-10 mt-3">
+                            <button class="btn btn-outline btn-success btn-sm" @click="downloadImage(img)">
+                                Загрузить
+                            </button>
+                            <button class="btn btn-square btn-outline btn-error btn-sm" @click="deleteImage(img)">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
                             </button>
                         </div>
                     </div>
-                    <img v-else class="w-32 h-32 cursor-pointer hover:scale-110 hover:opacity-50 transition-all delay-75"
-                         :src="img.url"
-                         :class="{'scale-110 outline': img.isActive}"
-                         @click="setImage(img)"
-                    >
                 </template>
             <!--- </TransitionGroup> --->
 
             <button class="text-3xl hover:scale-110 hover:opacity-90 transition-all delay-75"
-                    :disabled="indexImagesAvatar === Object.keys(imagesAvatar).length - 2 || Object.keys(imagesAvatar).length === 2"
-                    :class="{'opacity-0' : indexImagesAvatar === Object.keys(imagesAvatar).length - 2 || Object.keys(imagesAvatar).length === 2}"
-                    @click="indexImagesAvatar++">
+                    :disabled="indexDisplayImage === Object.keys(images).length - 2 || Object.keys(images).length === 2"
+                    :class="{'opacity-0' : indexDisplayImage === Object.keys(images).length - 2 || Object.keys(images).length === 2}"
+                    @click="indexDisplayImage++">
                 >
             </button>
 
