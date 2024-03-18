b33c3e8661f79bd4d1e61bf92ed914c0c94fb07c
plzDontT0uchMe <plzDontT0uchMe@proton.me>
2024-03-18 15:34:44 +0300

    test

---
+++ src/components/ModalConfirm.vue
@@ -0,0 +1,46 @@
+<script setup>
+const modal = defineModel();
+
+const getStyle = (element, type) =>
+{
+    const styles = {
+        'button': {
+            'download' : 'bg-green-600 hover:bg-green-700',
+            'delete' : 'bg-red-600 hover:bg-red-700'
+        }
+    };
+    return styles[element][type];
+}
+</script>
+
+<template>
+    <div class="fixed top-0 left-0 flex justify-center items-center w-screen h-screen backdrop-blur-[2px]">
+        <div class="p-6 bg-[#1D1D1D] border-black border-2 rounded-xl flex justify-around items-center flex-col">
+            <div class="text-xl mb-6">
+                <p>{{ modal.text }}</p>
+            </div>
+            <div class="w-64 h-64 mb-6">
+                <img :src="modal.img.url">
+            </div>
+            <div class="">
+                <button
+                    class="bg-gray-500 p-2 rounded-2xl mr-2 text-white"
+                    @click="modal.show = false"
+                >
+                    {{modal.buttons[0]}}
+                </button>
+                <button
+                    class="bg-gray-500 p-2 rounded-2xl ml-2 text-white"
+                    @click="modal.confirm = true"
+                    :class="getStyle('button', modal.action)"
+                >
+                    {{modal.buttons[1]}}
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/menuItems/MyAccount.vue
+++ src/components/menuItems/MyAccount.vue
@@ -1,8 +1,7 @@
 <script setup>
-import {computed, onMounted, ref} from "vue";
-import AvatarPreview from "@/components/menuPreviews/AvatarPreview.vue";
-import NewAvatarIcon from "@/components/menuIcons/NewAvatarIcon.vue";
+import {computed, onMounted, ref, watchEffect} from "vue";
 import axios from "@/axios";
+import ModalConfirm from "@/components/ModalConfirm.vue";
 
 const images = ref([
     {
@@ -39,6 +38,8 @@ const images = ref([
 
 const selectedImage = ref(0);
 
+const hoverImage = ref(null);
+
 onMounted(() => {
     initImages();
 })
@@ -60,7 +61,7 @@ const updateIndexImage = () => {
             indexDisplayImage.value = 1;
     }
 }
-const displayImagesAvatar = computed(() => {
+const displayImages = computed(() => {
     if(indexDisplayImage.value !== 0)
         return images.value.slice(indexDisplayImage.value - 1, indexDisplayImage.value - 1 + countDisplayImages);
     else
@@ -80,13 +81,31 @@ const loadImage = (event) => {
     setImage(img);
     myFile.value.value = ''
 }
+const dropImage = (event) => {
+    event.target.classList.remove(['opacity-50'], ['scale-110'], ['cursor-pointer'])
+    const file = event.dataTransfer.files[0];
+    const img = {
+        url: URL.createObjectURL(file),
+        file: file,
+        isLoading: true,
+        progress: 0
+    }
+    images.value.splice(1,0, img);
+    setImage(img);
+    myFile.value.value = ''
+}
 const setImage = (img) => {
     selectedImage.value = img;
     updateIndexImage();
 }
-const deleteImage = (img) => {
+const deleteLocalImage = (img) => {
+    URL.revokeObjectURL(img.url)
     images.value = images.value.filter((item) => item.url !== img.url)
-    console.log(indexDisplayImage.value)
+    updateIndexImage();
+}
+const deleteImage = (img) => {
+    URL.revokeObjectURL(img.url)
+    images.value = images.value.filter((item) => item.url !== img.url)
     updateIndexImage();
 }
 const downloadImage = async (img) => {
@@ -104,6 +123,39 @@ const downloadImage = async (img) => {
         console.log(e);
     }
 }
+
+const modal = ref({
+    show: false,
+    confirm: false,
+    img: null,
+    text: null,
+    action: null,
+    buttons: null
+});
+const showModal = (img, text, action, buttons) => {
+    modal.value = {
+        show: true,
+        confirm: false,
+        img: img,
+        text: text,
+        action: action,
+        buttons: buttons
+    }
+}
+watchEffect(async () => {
+    if(modal.value.confirm){
+        switch (modal.value.action){
+            case 'delete':{
+                deleteImage(modal.value.img)
+                break;
+            }
+            case 'download':{
+                await downloadImage(modal.value.img)
+                break;
+            }
+        }
+    }
+})
 </script>
 
 <template>
@@ -113,10 +165,11 @@ const downloadImage = async (img) => {
         <div class="text-[24px] mt-6 mb-6">Аватарки</div>
         <div class="flex flex-row justify-around items-center w-full select-none">
 
-            <button class="text-3xl hover:scale-110 hover:opacity-50 transition-all delay-75"
-                    :disabled="indexDisplayImage === 1 || Object.keys(images).length === 2"
-                    :class="{'opacity-0' : indexDisplayImage === 1 || Object.keys(images).length === 2}"
-                    @click="indexDisplayImage--"
+            <button
+                class="text-3xl hover:scale-110 hover:opacity-50 transition-all delay-75"
+                :disabled="indexDisplayImage === 1 || Object.keys(images).length === 2"
+                :class="{'opacity-0' : indexDisplayImage === 1 || Object.keys(images).length === 2}"
+                @click="indexDisplayImage--"
             >
                 <
             </button>
@@ -125,42 +178,87 @@ const downloadImage = async (img) => {
 
             <!--- <TransitionGroup name="list" tag="div" class="flex justify-around items-center flex-nowrap w-full"> --->
                 <template
-                    v-for="img in displayImagesAvatar"
+                    v-for="img in displayImages"
                     :key="img.id"
                 >
-                    <div v-if="img?.create" @click="myFile.click" class="btn btn-square btn-outline btn-sm flex justify-center items-center w-32 h-32">
+                    <div v-if="img?.create"
+                        class="btn btn-square btn-outline btn-sm flex justify-center items-center w-32 h-32"
+                        draggable="true"
+                        @click="myFile.click"
+                        @drop.prevent="dropImage"
+                        @dragover.prevent="$event.target.classList.add(['opacity-50'], ['scale-110'], ['cursor-pointer'])"
+                        @dragleave.prevent="$event.target.classList.remove(['opacity-50'], ['scale-110'], ['cursor-pointer'])"
+                    >
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
                     </div>
+
                     <div v-else-if="img?.empty" class="w-32 h-32"></div>
+
                     <div v-else class="relative">
-                        <img class="w-32 h-32 cursor-pointer hover:scale-110 hover:opacity-50 transition-all delay-75"
-                             :src="img.url"
-                             :class="{'scale-110 outline': img === selectedImage}"
-                             @click="setImage(img)"
+
+                        <div @mouseover="hoverImage === null ? hoverImage = img.url : hoverImage" @mouseleave="hoverImage = null">
+
+                            <img
+                                class="w-32 h-32 rounded-lg cursor-pointer hover:scale-110 hover:opacity-50 transition-all delay-75"
+                                :src="img.url"
+                                :class="{'scale-110 outline': img === selectedImage && !img?.isLoading, 'scale-100 outline': img?.isLoading, 'opacity-50': img?.progress > 0}"
+                                @click="setImage(img)"
+                                alt="image"
+                            >
+
+                            <div v-show="hoverImage === img.url && !img?.isLoading" class="absolute top-[-17px] right-[-17px]">
+                                <button class="btn btn-circle btn-outline btn-xs btn-error"
+                                        @click="showModal(img, 'Вы действительно хотите удалить?', 'delete', ['Отменить', 'Удалить'])">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                </button>
+                            </div>
+
+                        </div>
+
+                        <div v-if="img?.progress > 0"
+                             class="absolute left-0 bottom-0 rounded-lg w-full bg-[#00A872] transition-all delay-75"
+                             :style="{height: img?.progress + '%'}"
                         >
+                            <p class="absolute left-0 bottom-0 flex justify-center items-center text-white w-32 h-32">
+                                {{img?.progress.toFixed()}}%
+                            </p>
+                        </div>
+
                         <div v-if="img?.isLoading" class="absolute left-0 flex justify-between items-center w-full h-10 mt-3">
-                            <button class="btn btn-outline btn-success btn-sm" @click="downloadImage(img)">
+                            <button v-if="img?.progress === 0"
+                                class="btn btn-outline btn-success btn-sm w-[70%]"
+                                @click="showModal(img, 'Вы действительно хотите загрузить?', 'download', ['Отменить', 'Загрузить'])"
+                            >
                                 Загрузить
                             </button>
-                            <button class="btn btn-square btn-outline btn-error btn-sm" @click="deleteImage(img)">
+                            <button v-else-if="img?.progress > 0 && img?.progress < 100" class="btn btn-outline btn-success btn-sm w-[70%]">
+                                <span class="loading loading-spinner text-success"></span>
+                            </button>
+                            <button v-if="img?.progress === 100" class="btn btn-outline btn-success btn-sm w-[70%] disabled">
+                                ✓
+                            </button>
+                            <button class="btn btn-square btn-outline btn-error btn-sm" @click="deleteLocalImage(img)">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
                             </button>
                         </div>
+
                     </div>
+
                 </template>
             <!--- </TransitionGroup> --->
 
-            <button class="text-3xl hover:scale-110 hover:opacity-90 transition-all delay-75"
-                    :disabled="indexDisplayImage === Object.keys(images).length - 2 || Object.keys(images).length === 2"
-                    :class="{'opacity-0' : indexDisplayImage === Object.keys(images).length - 2 || Object.keys(images).length === 2}"
-                    @click="indexDisplayImage++">
+            <button
+                class="text-3xl hover:scale-110 hover:opacity-90 transition-all delay-75"
+                :disabled="indexDisplayImage === Object.keys(images).length - 2 || Object.keys(images).length === 2"
+                :class="{'opacity-0' : indexDisplayImage === Object.keys(images).length - 2 || Object.keys(images).length === 2}"
+                @click="indexDisplayImage++"
+            >
                 >
             </button>
 
         </div>
 
-        <!---<AvatarPreview v-model="newImageAvatar" v-if="isChangeImage" />--->
-
+        <ModalConfirm v-model="modal" v-if="modal.show && !modal.confirm" />
     </div>
 </template>
 
